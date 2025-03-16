a09563c2ebd2d15d7b07774b0d45e182b0b5d2a0
Chandra Tritaqwa Ramadhan <lolpotch@gmail.com>
2025-03-16 22:07:40 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,5 +10,5 @@ $routes->get('/users', 'UserController::index');
 $routes->get('/users/create', 'UserController::create');
 $routes->post('/users/store', 'UserController::store');
 $routes->get('/users/edit/(:num)', 'UserController::edit/$1');
-$routes->post('/users/update/(:num)', 'UserController::update/$1');
+$routes->post('/users/edit/update/(:num)', 'UserController::update/$1');
 $routes->get('/users/delete/(:num)', 'UserController::delete/$1');
